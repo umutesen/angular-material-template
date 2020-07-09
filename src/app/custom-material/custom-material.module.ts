6c665a5ace825d9c26af958e641dbb49710d13f9
Juergen Skrotzky <jorgenvikinggod@gmail.com>
2020-07-09 21:12:07 +0200

    Update angular material 9

--- src/app/custom-material/custom-material.module.ts
+++ src/app/custom-material/custom-material.module.ts
@@ -1,35 +1,33 @@
 import { NgModule, LOCALE_ID, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import {
-  MAT_DATE_FORMATS,
-  MatIconModule,
-  MatToolbarModule,
-  MatButtonModule,
-  MatListModule,
-  MatCardModule,
-  MatProgressBarModule,
-  MatInputModule,
-  MatSnackBarModule,
-  MatMenuModule,
-  MatSidenavModule,
-  MatProgressSpinnerModule,
-  MatDatepickerModule,
-  MatTableModule,
-  MatAutocompleteModule,
-  MatDialogModule,
-  MatTabsModule,
-  MatTooltipModule,
-  MatSelectModule,
-  MatPaginatorModule,
-  MatChipsModule,
-  MatButtonToggleModule,
-  MatSlideToggleModule,
-  MatBadgeModule,
-  MatCheckboxModule,
-  MatExpansionModule,
-  MatSortModule,
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { SelectCheckAllComponent } from './select-check-all/select-check-all.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
